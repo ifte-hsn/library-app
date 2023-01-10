39732eccbc9ee2cbc606e11cd75eb377159cd617
iftekhar_hossain <iftekhar.hossain@sonalibank.com.bd>
2023-01-10 15:57:10 +0600

    reviews on book checkout page

--- 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -3,12 +3,23 @@ import BookModel from "../../Models/BookModel";
 import {SpinnerLoading} from "../Utils/SpinnerLoading";
 import {StarsReview} from "../Utils/StarsReview";
 import {CheckoutAndReviewBox} from "./CheckoutAndReviewBox";
+import ReviewModel from "../../Models/ReviewModel";
+
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
+import {LatestReview} from "./LatestReview";
 
 export const BookCheckoutPage = () => {
 
     const [book, setBook] = useState<BookModel>();
     const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
+
+    // Review State
+    const [reviews, setReviews] = useState<ReviewModel[]>([]);
+    const [totalStars, setTotalStars] = useState(0);
+    const [isLoadingReview, setIsLoadingReview] = useState(true);
+
     const bookId = (window.location.pathname).split('/')[2];
 
 
@@ -46,7 +57,50 @@ export const BookCheckoutPage = () => {
         });
         window.scrollTo(0, 0);
     }, []);
-    if (isLoading) {
+
+    useEffect(() => {
+        const fetchBookReviews = async () => {
+            const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByBookId?bookId=${bookId}`;
+
+            const responseReviews = await fetch(reviewUrl);
+
+            if (!responseReviews.ok) {
+                throw new Error("Something went wrong!");
+            }
+
+            const responseJsonReviews = await responseReviews.json();
+            const responseData = responseJsonReviews._embedded.reviews;
+            const loadedReviews: ReviewModel[] = [];
+            let weightedStarReviews: number = 0;
+
+            for (const key in responseData) {
+                loadedReviews.push({
+                    id: responseData[key].id,
+                    userEmail: responseData[key].userEmail,
+                    date: responseData[key].date,
+                    rating: responseData[key].rating,
+                    book_id: responseData[key].bookId,
+                    reviewDescription: responseData[key].reviewDescription
+                });
+
+                weightedStarReviews = weightedStarReviews + responseData[key].rating;
+            }
+
+            if (loadedReviews) {
+                const round = (Math.round((weightedStarReviews / loadedReviews.length) * 2) / 2).toFixed(1);
+                setTotalStars(Number(round));
+            }
+
+            setReviews(loadedReviews);
+            setIsLoadingReview(false);
+        };
+        fetchBookReviews().catch((error: any) => {
+            setIsLoadingReview(false);
+            setHttpError(error.message);
+        })
+    }, [])
+
+    if (isLoading || isLoadingReview) {
         return (
             <SpinnerLoading/>
         )
@@ -77,13 +131,15 @@ export const BookCheckoutPage = () => {
                         <div className="ml-2">
                             <h2>{book?.title}</h2>
                             <h5 className="text-primary">{book?.author}</h5>
-                            <p className="lead">${book?.description}</p>
-                             <StarsReview rating={2.5} size={32} />
+                            <p className="lead">{book?.description}</p>
+                            <StarsReview rating={totalStars} size={32}/>
                         </div>
                     </div>
                     <CheckoutAndReviewBox book={book} mobile={false}/>
                 </div>
                 <hr/>
+
+                <LatestReview reviews={reviews} bookId={book?.id} mobile={false}/>
             </div>
 
             <div className="container d-lg-none mt-5">
@@ -100,12 +156,14 @@ export const BookCheckoutPage = () => {
                         <h2>{book?.title}</h2>
                         <h5 className="text-primary">{book?.author}</h5>
                         <p className="lead">${book?.description}</p>
-                        <StarsReview rating={2.5} size={32} />
+                        <StarsReview rating={totalStars} size={32}/>
                     </div>
 
                 </div>
                 <CheckoutAndReviewBox book={book} mobile={true}/>
                 <hr/>
+                <LatestReview reviews={reviews} bookId={book?.id} mobile={true}/>
+
             </div>
         </div>
     )
---
+++ 03-frontend/react-library/src/layouts/Utils/Review.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReviewModel from "../../Models/ReviewModel";
+import {StarsReview} from "./StarsReview";
+
+export const Review: React.FC<{review: ReviewModel}> = (props) => {
+    const date = new Date(props.review.date);
+    const longMonth = date.toLocaleDateString('en-us', {month: 'long'});
+    const dateDay = date.getDate();
+    const dateYear = date.getFullYear();
+
+    const dateRender = longMonth + " " + dateDay + ", "+dateYear;
+
+    return (
+        <div>
+            <div className="col-sm-8 col-md-8">
+                <h4>{props.review.userEmail}</h4>
+                <div className="row">
+                    <div className="col">
+                        {dateRender}
+                    </div>
+                    <div className="col">
+                        <StarsReview rating={props.review.rating} size={16} />
+                    </div>
+                </div>
+
+                <div className="mt-2">
+                    <p>
+                        {props.review.reviewDescription}
+                    </p>
+                </div>
+            </div>
+            <hr/>
+        </div>
+    )
+}
